fdff5d3ac80e7aeb1b7a9b9ebeb5b3c0a1bb1d35
julietafoglia <julieta.foglia@valtech.com>
2020-07-02 21:54:12 -0300

    feature/DEV-69: se agrega envio de password, transparente al usuario

--- front/src/components/videollamada/videollamada.js
+++ front/src/components/videollamada/videollamada.js
@@ -20,7 +20,6 @@ const Videollamada = ({
 }) => {
   const { microfono, camara } = options;
   const parentNode = 'jitsi-container';
-
   const setElementHeight = () => {
     const element = document.querySelector(`#${parentNode}`);
     if (element) {
