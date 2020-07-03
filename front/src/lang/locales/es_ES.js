bbafc4aacb7a8c976b1159370b8c206c9824cc70
julietafoglia <julieta.foglia@valtech.com>
2020-07-03 16:21:03 -0300

    se remueve archivo agregado erroneamente

--- front/src/lang/locales/es_ES.js
+++ front/src/lang/locales/es_ES.js
@@ -10,80 +10,84 @@ Tabla de Contenidos
 06.Virtual Classes
   06.01.My Classes
 07.Institution Selection
-07.Course Selection
-*/ 
+08.Course Selection
+09.Prácticas
+*/
 
 module.exports = {
   /* 01.General */
 
   /* 02.Inicio de sesión de usuario, cierre de sesión, registro */
-  "usuario.login-title": "Iniciar sesión",
-  "user.register": "Registro",
-  "user.forgot-password": "Reinicie su contraseña",
-  "user.email": "Email",
-  "user.password": "Contraseña",
-  "user.forgot-password-question": "¿Contraseña olvidada?",
-  "user.fullname": "Nombre completo",
-  "user.login-button": "INICIAR SESIÓN",
-  "user.register-button": "REGISTRO",
-  "user.reset-password-button": "REINICIAR",
-  "user.buy": "COMPRAR",
-  "user.username": "Nombre de Usuario",
+  'usuario.login-title': 'Iniciar sesión',
+  'user.register': 'Registro',
+  'user.forgot-password': 'Reinicie su contraseña',
+  'user.email': 'Email',
+  'user.password': 'Contraseña',
+  'user.forgot-password-question': '¿Contraseña olvidada?',
+  'user.fullname': 'Nombre completo',
+  'user.login-button': 'INICIAR SESIÓN',
+  'user.register-button': 'REGISTRO',
+  'user.reset-password-button': 'REINICIAR',
+  'user.buy': 'COMPRAR',
+  'user.username': 'Nombre de Usuario',
 
   /* 03.Menú */
-  "menu.app": "Inicio",
-  "menu.dashboards": "Tableros",
-  "menu.inicio": "Inicio",
-  "menu.start": "¡Hola!",
-  "menu.second-menu": "Segundo menú",
-  "menu.second": "Segundo",
-  "menu.ui": "IU",
-  "menu.charts": "Gráficos",
-  "menu.chat": "Chatea",
-  "menu.survey": "Encuesta",
-  "menu.todo": "Notas",
-  "menu.search": "Búsqueda",
-  "menu.docs": "Docs",
-  "menu.blank-page": "Blank Page",
-  "menu.communications": "Comunicaciones",
-  "menu.content": "Contenidos",
-  "menu.corrections": "Correcciones",
-  "menu.virtual-classes": "Clases Virtuales",
-  "menu.activities": "Prácticas",
-  "menu.evaluations": "Evaluaciones",
-  "menu.forum": "Foro",
-  "menu.messages": "Mensajería",
-  "menu.formal": "Comunicaciones Formales",
-  "menu.my-contents": "Mis Contenidos",
-  "menu.my-corrections": "Mis Correcciones",
-  "menu.my-classes": "Mis Clases",
-  "menu.board": "Pizarrón",
-  "menu.my-activities": "Mis Prácticas",
-  "menu.my-evaluations": "Mis Evaluaciones",
+  'menu.app': 'Inicio',
+  'menu.dashboards': 'Tableros',
+  'menu.inicio': 'Inicio',
+  'menu.start': '¡Hola!',
+  'menu.second-menu': 'Segundo menú',
+  'menu.second': 'Segundo',
+  'menu.ui': 'IU',
+  'menu.charts': 'Gráficos',
+  'menu.chat': 'Chatea',
+  'menu.survey': 'Encuesta',
+  'menu.todo': 'Notas',
+  'menu.search': 'Búsqueda',
+  'menu.docs': 'Docs',
+  'menu.blank-page': 'Blank Page',
+  'menu.communications': 'Comunicaciones',
+  'menu.content': 'Contenidos',
+  'menu.corrections': 'Correcciones',
+  'menu.virtual-classes': 'Clases Virtuales',
+  'menu.activities': 'Prácticas',
+  'menu.evaluations': 'Evaluaciones',
+  'menu.forum': 'Foro',
+  'menu.messages': 'Mensajería',
+  'menu.formal': 'Comunicaciones Formales',
+  'menu.my-contents': 'Mis Contenidos',
+  'menu.my-corrections': 'Mis Correcciones',
+  'menu.my-classes': 'Mis Clases',
+  'menu.board': 'Pizarrón',
+  'menu.my-activities': 'Mis Prácticas',
+  'menu.my-evaluations': 'Mis Evaluaciones',
 
   /* 04.Error  */
-  "pages.error-title": "Vaya, parece que ha ocurrido un error!",
-  "pages.error-code": "Código de error",
-  "pages.go-back-home": "Regresar a Inicio",
+  'pages.error-title': 'Vaya, parece que ha ocurrido un error!',
+  'pages.error-code': 'Código de error',
+  'pages.go-back-home': 'Regresar a Inicio',
 
   /* 06.Pages general */
-  "pages.orderby" : "Ordenar por",
-  "pages.delete" : "Eliminar",
-  "pages.submit": "Aceptar",
-  "pages.cancel": "Cancelar",
+  'pages.orderby': 'Ordenar por',
+  'pages.delete': 'Eliminar',
+  'pages.submit': 'Aceptar',
+  'pages.cancel': 'Cancelar',
 
   /* 06.Virtual Classes */
 
-    /* 06.01.My Classes */
-    "classes.add" : "AGREGAR CLASE",
-    "classes.edit" : "Editar",
-    "class.name": "Nombre",
-    "class.level": "Nivel educativo",
-    "class.description": "Descripción",
+  /* 06.01.My Classes */
+  'classes.add': 'AGREGAR CLASE',
+  'classes.edit': 'Editar',
+  'class.name': 'Nombre',
+  'class.level': 'Nivel educativo',
+  'class.description': 'Descripción',
 
   /* 07.Institution Selection */
-    "institution.selection" : "¿A qué institución desea acceder?",
+  'institution.selection': '¿A qué institución desea acceder?',
 
-  /* 07.Course Selection */
-  "course.selection" : "¿Qué materia desea gestionar?",
+  /* 08.Course Selection */
+  'course.selection': '¿Qué materia desea gestionar?',
+
+  /* 08.Prácticas */
+  'activity.add': 'AGREGAR PRÁCTICA',
 };
