32c29784ceaec55ecc06d65c4c7487f088a08766
Micaela De Rito <mderito@circostudio.com>
2020-07-12 21:56:38 -0300

    componente arbol de archivos

--- front/src/pages/app/contenidos/contenidos.js
+++ front/src/pages/app/contenidos/contenidos.js
@@ -4,6 +4,10 @@ import { injectIntl } from 'react-intl';
 import HeaderDeModulo from 'components/common/HeaderDeModulo';
 import contents from 'data/contents';
 import Contents from 'containers/dashboards/contents';
+import FileBrowser, { Icons } from 'react-keyed-file-browser';
+import { storage } from 'helpers/Firebase';
+import Moment from 'moment';
+import '../../../../node_modules/react-keyed-file-browser/dist/react-keyed-file-browser.css';
 
 function collect(props) {
   return { data: props.data };
@@ -14,7 +18,43 @@ class Contenidos extends Component {
     super(props);
 
     this.state = {
-      items: [],
+      files: [
+        {
+          key: 'photos/animals/cat in a hat.png',
+          modified: +Moment().subtract(1, 'hours'),
+          size: 1.5 * 1024 * 1024,
+        },
+        {
+          key: 'photos/animals/kitten_ball.png',
+          modified: +Moment().subtract(3, 'days'),
+          size: 545 * 1024,
+        },
+        {
+          key: 'photos/animals/elephants.png',
+          modified: +Moment().subtract(3, 'days'),
+          size: 52 * 1024,
+        },
+        {
+          key: 'photos/funny fall.gif',
+          modified: +Moment().subtract(2, 'months'),
+          size: 13.2 * 1024 * 1024,
+        },
+        {
+          key: 'photos/holiday.jpg',
+          modified: +Moment().subtract(25, 'days'),
+          size: 85 * 1024,
+        },
+        {
+          key: 'documents/letter chunks.doc',
+          modified: +Moment().subtract(15, 'days'),
+          size: 480 * 1024,
+        },
+        {
+          key: 'documents/export.pdf',
+          modified: +Moment().subtract(15, 'days'),
+          size: 4.2 * 1024 * 1024,
+        },
+      ],
       selectedItems: [],
       isLoading: true,
     };
@@ -22,7 +62,7 @@ class Contenidos extends Component {
 
   // Esto va a hacer el get del storage para la materia
   getContenidos = async () => {
-    this.dataListRenderer(contents);
+    this.dataListRenderer();
   };
 
   componentDidMount() {
@@ -41,9 +81,29 @@ class Contenidos extends Component {
     this.getContenidos();
   };
 
-  dataListRenderer(arrayDeObjetos) {
+  dataListRenderer() {
+    var array = [];
+    var subject = localStorage.getItem('subject');
+    // Create a reference under which you want to list
+    var listRef = storage.ref(subject.id);
+
+    // Find all the prefixes and items.
+    listRef
+      .listAll()
+      .then(function (res) {
+        res.prefixes.forEach(function (folderRef) {
+          // All the prefixes under listRef.
+          // You may call listAll() recursively on them.
+        });
+        res.items.forEach(function (itemRef) {
+          // All the items under listRef.
+        });
+      })
+      .catch(function (error) {
+        // Uh-oh, an error occurred!
+      });
     this.setState({
-      items: arrayDeObjetos,
+      items: array,
       selectedItems: [],
       isLoading: false,
     });
@@ -62,11 +122,23 @@ class Contenidos extends Component {
             buttonText="contenido.agregar"
           />
           <Row>
-            <Contents data={contents} />{' '}
+            <FileBrowser
+              files={this.state.files}
+              icons={Icons.FontAwesome(4)}
+              onCreateFolder={this.handleCreateFolder}
+              onCreateFiles={this.handleCreateFiles}
+              onMoveFolder={this.handleRenameFolder}
+              onMoveFile={this.handleRenameFile}
+              onRenameFolder={this.handleRenameFolder}
+              onRenameFile={this.handleRenameFile}
+              onDeleteFolder={this.handleDeleteFolder}
+              onDeleteFile={this.handleDeleteFile}
+            />
           </Row>
         </div>
       </Fragment>
     );
   }
 }
-export default injectIntl(Contenidos);
+const mount = document.querySelectorAll('div.demo-mount-nested-editable');
+export default injectIntl(Contenidos, mount[0]);
