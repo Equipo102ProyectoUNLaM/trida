4fbc61d56fa2d063bb5879f8f9c66fcfa4f050b6
julieta-foglia <juli.foglia@gmail.com>
2020-07-16 21:45:17 -0300

    feature/DEV-84: se corrige agregado de práctica

--- front/src/pages/app/practicas/form-practica.js
+++ front/src/pages/app/practicas/form-practica.js
@@ -50,6 +50,7 @@ class FormPractica extends React.Component {
   handleSubmit = async (event) => {
     event.preventDefault();
     if (this.props.operationType === 'add') {
+      const { id } = JSON.parse(localStorage.getItem('subject'));
       const obj = {
         nombre: this.state.nombre,
         fechaLanzada: this.state.fechaLanzada,
@@ -57,7 +58,7 @@ class FormPractica extends React.Component {
         duracion: this.state.duracion,
         fechaVencimiento: this.state.fechaVencimiento,
         fechaPublicada: new Date(),
-        idMateria: JSON.parse(localStorage.getItem('subject')),
+        idMateria: id,
       };
       await addDocument('practicas', obj, 'Práctica');
     } else {
