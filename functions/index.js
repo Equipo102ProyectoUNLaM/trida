9e9d4033a641dd30df22592bee75981a87178f13
julieta-foglia <juli.foglia@gmail.com>
2020-07-31 22:50:14 -0300

    feature/DEV-133: probando mail invitacion

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,18 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp(functions.config().firebase)
+const nodemailer = require('nodemailer');
+const cors = require('cors')({origin: true});
+admin.initializeApp();
 
 /* healthcheck endpoint */
 exports.ping = functions.https.onRequest((request, response) => {
+  cors(request, response, () => {
   response.send("pong");
+  });
  });
 
  /* create user creates a user into firebase auth service */
-exports.createUser = functions.https.onRequest( (data, context) => {
+/*exports.createUser = functions.https.onRequest( (data, context) => {
    console.log('Name', data.name);
     return admin.auth().createUser({
         email: data.mail,
@@ -19,19 +23,19 @@ exports.createUser = functions.https.onRequest( (data, context) => {
         photoURL: 'http://www.example.com/12345678/photo.png',
         disabled: false
       })
-        .then(function(userRecord) {
+        .then((userRecord) => {
           // See the UserRecord reference doc for the contents of userRecord.
           console.log('Successfully created new user:', userRecord.uid);
           return userRecord
         })
-        .catch(function(error) {
+        .catch((error) => {
           console.log('Error creating new user:', error);
           return error
         });
 })
 
 /* register sets the user record to firestore */
-exports.register = functions.auth.user().onCreate((data)=> {
+/*exports.register = functions.auth.user().onCreate((data)=> {
     const user = {
         id: data.uid,
         name: data.displayName,
@@ -43,5 +47,34 @@ exports.register = functions.auth.user().onCreate((data)=> {
     .doc(data.uid)
     .set(user)
     .then(doc => console.log('user added', doc))   
-})
+})*/
 
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  //port: 465,
+  auth: {
+      user: 'trida.app@gmail.com',
+      pass: 'tr1d@@pp'
+  }
+});
+
+exports.sendMail = functions.https.onRequest((req, res) => {
+  //cors(req, response, () => {
+    
+      // getting dest email by query string
+      const dest = req.query.dest;
+      console.log(dest);
+      const mailOptions = {
+          from: 'Trida App <trida.app@gmail.com>',
+          to: dest,
+          subject: 'Te invitaron a Trida!',
+          html: `<p style="font-size: 16px;">En instantes recibirás un mail para establecer tu contraseña. <br />
+          Luego, podés loguearte con este mail y tu contraseña en trida.com.ar</p>
+              <br />
+          `
+      };
+
+      // returning result
+    return transporter.sendMail(mailOptions);
+ // });    
+});
