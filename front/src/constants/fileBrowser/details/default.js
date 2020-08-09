c7bc1ae1d56df3414e38e616f815a1e899ba9998
Thomas Reynoso <thomas@mudafy.com>
2020-08-09 02:54:33 -0300

    se obtienen mas datos que se muestran en el detalle

--- front/src/constants/fileBrowser/details/default.js
+++ front/src/constants/fileBrowser/details/default.js
@@ -9,9 +9,7 @@ class Detail extends React.Component {
       name: PropTypes.string.isRequired,
       extension: PropTypes.string.isRequired,
       url: PropTypes.string,
-      fullKey: PropTypes.string,
     }).isRequired,
-    //fullKey: PropTypes.string,
     close: PropTypes.func,
   };
 
@@ -20,23 +18,41 @@ class Detail extends React.Component {
     const { id } = JSON.parse(localStorage.getItem('subject'));
 
     this.state = {
-      file: 'mjQtdNYdRKMqZ3aty4NKBh7ZXjG2-00---Presentacion-de-la-materia1.pdf',
       isLoading: true,
       subjectId: id,
       alumno: '',
+      estado: '',
+      fecha_entrega: '',
+      fecha_vto_entrega: '',
     };
   }
 
   componentDidMount = async () => {
-    const correccion = await this.getCorreccion();
-    this.getAlumno(correccion[0].data.id_alumno);
+    const keyFile = this.getKeyFile();
+    const correccion = await this.getCorreccion(keyFile);
+    this.setEstadoYFecha(correccion);
+    this.setFechaVtoEntrega(correccion);
+    this.getAlumno(correccion.id_alumno);
   };
 
-  getCorreccion = async () => {
-    return await getCollection('correcciones', [
-      { field: 'key', operator: '==', id: this.state.file },
+  getKeyFile() {
+    const busquedaIni = '%2Fcorrecciones%2F';
+    const longbusquedaIni = busquedaIni.length;
+    const busquedaFin = '?alt';
+    const url = this.props.file.url;
+    return url.substring(
+      url.lastIndexOf(busquedaIni) + longbusquedaIni,
+      url.lastIndexOf(busquedaFin)
+    );
+  }
+
+  getCorreccion = async (keyFile) => {
+    const correccion = await getCollection('correcciones', [
+      { field: 'key', operator: '==', id: keyFile },
       { field: 'id_materia', operator: '==', id: this.state.subjectId },
     ]);
+
+    return correccion[0].data;
   };
 
   getAlumno = async (idAlumno) => {
@@ -55,6 +71,36 @@ class Detail extends React.Component {
     this.props.close();
   };
 
+  setEstadoYFecha(correccion) {
+    this.setState({
+      estado: correccion.estado,
+      fecha_entrega: correccion.fecha_entrega,
+    });
+  }
+
+  setFechaVtoEntrega(correccion) {
+    if (correccion.tipo === 'practica') {
+      this.getFechaPractica(correccion.id_entrega);
+    } else if (correccion.tipo === 'evaluacion') {
+      this.getFechaEvaluacion(correccion.id_entrega);
+    }
+  }
+
+  getFechaPractica = async (idPractica) => {
+    const practicaObj = await getDocument(`practicas/${idPractica}`);
+    this.setState({
+      fecha_vto_entrega: practicaObj.data.fechaVencimiento,
+    });
+  };
+
+  //REVISAR
+  getFechaEvaluacion = async (idEvaluacion) => {
+    const evalObj = await getDocument(`practicas/${idEvaluacion}`);
+    this.setState({
+      fecha_vto_entrega: evalObj.data.fecha_finalizacion,
+    });
+  };
+
   probando = (event) => {
     alert('Hola');
     console.log('Hola');
@@ -63,7 +109,13 @@ class Detail extends React.Component {
   render() {
     let name = this.props.file.key.split('/');
     name = name.length ? name[name.length - 1] : '';
-    const { isLoading, alumno } = this.state;
+    const {
+      isLoading,
+      alumno,
+      estado,
+      fecha_entrega,
+      fecha_vto_entrega,
+    } = this.state;
 
     return (
       <Fragment>
@@ -71,12 +123,17 @@ class Detail extends React.Component {
         <div className="item-detail">
           <h2>Detalle de la corrección</h2>
           <dl>
-            <dt>Key</dt>
+            <dt>Nombre del archivo</dt>
             <dd>{this.props.file.key}</dd>
-            <dd>{this.props.fullKey}</dd>
             {('props', console.log(this.props))}
-            <dt>Nombre</dt>
+            <dt>Alumno </dt>
             <dd>{alumno}</dd>
+            <dt>Estado </dt>
+            <dd>{estado}</dd>
+            <dt>Fecha Entrega </dt>
+            <dd>{fecha_entrega}</dd>
+            <dt>Fecha Vencimiento de la Entrega </dt>
+            <dd>{fecha_vto_entrega}</dd>
           </dl>
           <a href="#" onClick={this.probando}>
             Probando
