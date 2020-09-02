37fee556d01c23581f89aae8cf6f303864c83421
julietafoglia <julieta.foglia@valtech.com>
2020-09-01 23:07:07 -0300

    se rediseña home docente

---
+++ front/src/containers/pages/DataListEventos.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Card, Row } from 'reactstrap';
+import classnames from 'classnames';
+import { ContextMenuTrigger } from 'react-contextmenu';
+import { Colxx } from '../../components/common/CustomBootstrap';
+import { NavLink } from 'react-router-dom';
+import Calendario from 'components/common/Calendario';
+import { injectIntl } from 'react-intl';
+import { editDocument } from 'helpers/Firebase-db';
+
+class DataListEventos extends React.Component {
+  handleClick = async (date) => {
+    if (date) {
+      const obj = { fechaVencimiento: date.format('YYYY-MM-DD') };
+      if (date) {
+        await editDocument('practicas', this.props.id, obj, 'Práctica');
+      }
+    }
+  };
+
+  render() {
+    const { id, title, isSelect, navTo } = this.props;
+    return (
+      <Colxx xxs="12" className="mb-3">
+        <ContextMenuTrigger id="menu_id" data={id}>
+          <Card
+            className={classnames('d-flex flex-row', {
+              active: isSelect,
+            })}
+          >
+            <div className="pl-2 d-flex flex-grow-1 min-width-zero">
+              <NavLink to={`${navTo}`} className="w-90 w-sm-100 active">
+                <div className="card-title-evento align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
+                  <p className="list-item-heading mb-1 truncate">{title}</p>
+                  <i className="simple-icon-arrow-right-circle" />
+                </div>
+              </NavLink>
+            </div>
+          </Card>
+        </ContextMenuTrigger>
+      </Colxx>
+    );
+  }
+}
+
+export default injectIntl(DataListEventos);
--- front/src/pages/app/home/inicio.js
+++ front/src/pages/app/home/inicio.js
@@ -1,19 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Row } from 'reactstrap';
+import { Row, Card, CardTitle, CardBody } from 'reactstrap';
 import ReactStickies from 'react-stickies';
 import IntlMessages from 'helpers/IntlMessages';
 import { Colxx, Separator } from 'components/common/CustomBootstrap';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
+import {
+  getEventos,
+  getEventosDelDia,
+  guardarNotas,
+  getDocument,
+} from 'helpers/Firebase-db';
 import moment from 'moment';
 import 'moment/locale/es.js';
-const myEventsList = [
-  {
-    title: 'Evento',
-    start: new Date('2020-09-05 08:00:00'),
-    end: new Date('2020-09-05 10:00:00'),
-  },
-];
+import DataListEventos from 'containers/pages/DataListEventos';
+import { nota } from 'constants/notas';
+
 const minTime = new Date();
 minTime.setHours(8, 0, 0);
 const maxTime = new Date();
@@ -25,10 +27,54 @@ class Inicio extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: false,
+      isLoading: true,
       notas: [],
+      eventos: [],
+      eventosDia: [],
     };
   }
+
+  componentDidMount() {
+    this.getEventos();
+    this.setNotas();
+  }
+
+  componentWillUnmount() {
+    const arrayTextos = this.state.notas.map((note) => {
+      return note.text;
+    });
+    guardarNotas(this.props.user, arrayTextos);
+  }
+
+  setNotas = async () => {
+    const arrayDeNotas = [];
+    const { data } = await getDocument(`notas/${this.props.user}`);
+    if (data) {
+      data.notas.forEach((note) => {
+        arrayDeNotas.push({
+          ...nota,
+          text: note,
+        });
+      });
+    } else {
+      arrayDeNotas.push({
+        ...nota,
+      });
+    }
+    this.setState({
+      notas: arrayDeNotas,
+    });
+  };
+
+  getEventos = async () => {
+    const eventos = await getEventos(this.props.subject.id);
+    const eventosDia = await getEventosDelDia(this.props.subject.id);
+    this.setState({
+      isLoading: false,
+      eventos,
+      eventosDia,
+    });
+  };
 
   onChangeNota = (notas) => {
     this.setState({
@@ -37,7 +83,7 @@ class Inicio extends Component {
   };
 
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, eventos, eventosDia } = this.state;
     return isLoading ? (
       <div className="cover-spin" />
     ) : (
@@ -50,41 +96,69 @@ class Inicio extends Component {
             <Separator className="mb-5" />
           </Colxx>
         </Row>
-        <Row className="row-home">
-          <ReactStickies
-            notes={this.state.notas}
-            onChange={this.onChangeNota}
-            footer={false}
-          />
-          <Calendar
-            culture="es-ES"
-            localizer={localizer}
-            events={myEventsList}
-            min={minTime}
-            max={maxTime}
-            startAccessor="start"
-            endAccessor="end"
-            className="calendar-home"
-            messages={{
-              next: '>',
-              previous: '<',
-              today: 'Hoy',
-              month: 'Mes',
-              week: 'Semana',
-              day: 'Día',
-            }}
-          />
+        <Row>
+          <Colxx>
+            <Card>
+              <CardTitle className="card-title-evento">
+                <i className="margin-right-icon simple-icon-event" />
+                Eventos del día
+              </CardTitle>
+              <CardBody className="card-body-evento">
+                {eventosDia.map((evento) => {
+                  return (
+                    <DataListEventos
+                      key={evento.id + 'dataList'}
+                      id={evento.id}
+                      title={evento.title}
+                      navTo={`/app/${evento.tipo}`}
+                    />
+                  );
+                })}{' '}
+              </CardBody>
+            </Card>
+            <ReactStickies
+              notes={this.state.notas}
+              onChange={this.onChangeNota}
+              footer={false}
+            />
+          </Colxx>
+          <Colxx>
+            <Row className="row-home">
+              <Card className="card-home">
+                <Calendar
+                  culture="es-ES"
+                  localizer={localizer}
+                  events={eventos}
+                  min={minTime}
+                  max={maxTime}
+                  startAccessor="start"
+                  endAccessor="end"
+                  className="calendar-home"
+                  messages={{
+                    next: '>',
+                    previous: '<',
+                    today: 'Hoy',
+                    month: 'Mes',
+                    week: 'Semana',
+                    day: 'Día',
+                  }}
+                />
+              </Card>
+            </Row>
+          </Colxx>
         </Row>
       </Fragment>
     );
   }
 }
 
-const mapStateToProps = ({ authUser }) => {
+const mapStateToProps = ({ authUser, seleccionCurso }) => {
   const { user } = authUser;
+  const { subject } = seleccionCurso;
 
   return {
     user,
+    subject,
   };
 };
 
