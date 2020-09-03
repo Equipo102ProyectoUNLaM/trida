d765fe2e49be558fbedd11cc54d4ddb66e68ac35
julietafoglia <julieta.foglia@valtech.com>
2020-09-03 15:45:13 -0300

    feature/DEV-179: se agrega edicion y borrado de clases

--- front/src/pages/app/clases-virtuales/clases/clases.js
+++ front/src/pages/app/clases-virtuales/clases/clases.js
@@ -5,11 +5,16 @@ import { injectIntl } from 'react-intl';
 import HeaderDeModulo from 'components/common/HeaderDeModulo';
 import ListaConImagen from 'components/lista-con-imagen';
 import ModalGrande from 'containers/pages/ModalGrande';
+import ModalConfirmacion from 'containers/pages/ModalConfirmacion';
 import FormClase from './form-clase';
-import { getCollection } from 'helpers/Firebase-db';
+import {
+  getCollection,
+  logicDeleteDocument,
+  getDocument,
+} from 'helpers/Firebase-db';
 import ROLES from 'constants/roles';
 const publicUrl = process.env.PUBLIC_URL;
-const imagenClase = `${publicUrl}/assets/img/imagen-clase.jpeg`;
+const imagenClase = `${publicUrl}/assets/img/imagen-clase-2.png`;
 
 function collect(props) {
   return { data: props.data };
@@ -25,12 +30,21 @@ class Clase extends Component {
       selectedItems: [],
       isLoading: true,
       idMateria: this.props.subject.id,
+      modalDeleteOpen: false,
+      modalEditOpen: false,
+      idClase: '',
+      idClaseEditada: '',
+      nombreClaseEditada: '',
+      descClaseEditada: '',
+      fechaClaseEditada: '',
+      salaEditada: false,
     };
   }
 
   getClases = async (materiaId) => {
     const arrayDeObjetos = await getCollection('clases', [
       { field: 'idMateria', operator: '==', id: materiaId },
+      { field: 'activo', operator: '==', id: true },
     ]);
     this.dataListRenderer(arrayDeObjetos);
   };
@@ -45,8 +59,9 @@ class Clase extends Component {
     });
   };
 
-  onClaseAgregada = () => {
-    this.toggleModal();
+  onClaseGuardada = () => {
+    if (this.state.modalEditOpen) this.toggleEditModal();
+    else this.toggleModal();
     this.getClases(this.state.idMateria);
   };
 
@@ -58,8 +73,60 @@ class Clase extends Component {
     });
   }
 
+  toggleDeleteModal = () => {
+    this.setState({
+      modalDeleteOpen: !this.state.modalDeleteOpen,
+    });
+  };
+
+  toggleEditModal = () => {
+    this.setState({
+      modalEditOpen: !this.state.modalEditOpen,
+    });
+  };
+
+  onDelete = (idClase) => {
+    this.setState({
+      idClase,
+    });
+    this.toggleDeleteModal();
+  };
+
+  onEdit = async (idClase) => {
+    const { data } = await getDocument(`clases/${idClase}`);
+    const { nombre, descripcion, fecha, idSala } = data;
+    this.setState({
+      idClaseEditada: idClase,
+      nombreClaseEditada: nombre,
+      descClaseEditada: descripcion,
+      fechaClaseEditada: fecha,
+      salaEditada: idSala ? true : false,
+    });
+    this.toggleEditModal();
+  };
+
+  borrarClase = async () => {
+    await logicDeleteDocument('clases', this.state.idClase, 'Clase');
+    this.setState({
+      idClase: '',
+    });
+    this.toggleDeleteModal();
+    this.getClases(this.state.idMateria);
+  };
+
   render() {
-    const { modalOpen, items, isLoading } = this.state;
+    const {
+      modalOpen,
+      items,
+      isLoading,
+      modalDeleteOpen,
+      modalEditOpen,
+      idClaseEditada,
+      nombreClaseEditada,
+      descClaseEditada,
+      fechaClaseEditada,
+      salaEditada,
+    } = this.state;
     const { rol } = this.props;
     return isLoading ? (
       <div className="loading" />
@@ -78,7 +145,7 @@ class Clase extends Component {
           >
             <FormClase
               toggleModal={this.toggleModal}
-              onClaseAgregada={this.onClaseAgregada}
+              onClaseGuardada={this.onClaseGuardada}
             />
           </ModalGrande>
           <Row>
@@ -91,10 +158,40 @@ class Clase extends Component {
                   isSelect={this.state.selectedItems.includes(clase.id)}
                   collect={collect}
                   navTo={`/app/clases-virtuales/mis-clases/detalle-clase/${clase.id}`}
+                  onEdit={this.onEdit}
+                  onDelete={this.onDelete}
                 />
               );
             })}{' '}
           </Row>
+          {modalEditOpen && (
+            <ModalGrande
+              modalOpen={modalEditOpen}
+              toggleModal={this.toggleEditModal}
+              modalHeader="classes.edit"
+            >
+              <FormClase
+                toggleModal={this.toggleEditModal}
+                onClaseGuardada={this.onClaseGuardada}
+                idClase={idClaseEditada}
+                nombre={nombreClaseEditada}
+                fecha={fechaClaseEditada}
+                descripcion={descClaseEditada}
+                sala={salaEditada}
+              />
+            </ModalGrande>
+          )}
+          {modalDeleteOpen && (
+            <ModalConfirmacion
+              texto="¿Estás seguro de borrar la clase?"
+              titulo="Borrar Clase"
+              buttonPrimary="Aceptar"
+              buttonSecondary="Cancelar"
+              toggle={this.toggleDeleteModal}
+              isOpen={modalDeleteOpen}
+              onConfirm={this.borrarClase}
+            />
+          )}
         </div>
       </Fragment>
     );
--- front/src/pages/app/clases-virtuales/clases/form-clase.js
+++ front/src/pages/app/clases-virtuales/clases/form-clase.js
@@ -1,58 +1,65 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { connect } from 'react-redux';
 import { Input, ModalFooter, Button, FormGroup, Label } from 'reactstrap';
 import Switch from 'rc-switch';
 import { createUUID } from 'helpers/Utils';
-import { addDocument } from 'helpers/Firebase-db';
+import { editDocument, addDocument } from 'helpers/Firebase-db';
 import * as CryptoJS from 'crypto-js';
 import { secretKey } from 'constants/defaultValues';
+import { capitalizeString } from 'helpers/Utils';
 
-const FormClase = ({ toggleModal, onClaseAgregada, subject, user }) => {
+const FormClase = ({
+  toggleModal,
+  onClaseGuardada,
+  subject,
+  user,
+  idClase,
+  nombre,
+  fecha,
+  descripcion,
+  sala,
+}) => {
   const { handleSubmit, errors, control } = useForm();
-  const [switchVideollamada, setSwitchVideollamada] = useState(false);
+  const [switchVideollamada, setSwitchVideollamada] = useState(sala);
 
   const onSubmit = async (values) => {
     let idSala = '';
     const { nombre, fecha, descripcion } = values;
-
     if (switchVideollamada) {
       const uuid = createUUID();
       idSala = CryptoJS.AES.encrypt(uuid, secretKey).toString();
     }
     const obj = {
-      nombre,
+      nombre: capitalizeString(nombre),
       fecha,
       descripcion,
       idSala,
       idMateria: subject.id,
       contenidos: [],
     };
-    await addDocument(
-      'clases',
-      obj,
-      user,
-      'Clase agregada',
-      'Clase agregada exitosamente',
-      'Error al agregar la clase'
-    );
 
-    onClaseAgregada();
-  };
+    if (idClase) {
+      await editDocument(
+        'clases',
+        idClase,
+        obj,
+        'Clase',
+        'Clase',
+        'Error al guardar la clase'
+      );
+    } else {
+      await addDocument(
+        'clases',
+        obj,
+        user,
+        'Clase agregada',
+        'Clase agregada exitosamente',
+        'Error al agregar la clase'
+      );
+    }
 
-  const generateIdSala = () => {
-    this.setState(
-      (prevState) => ({
-        switchVideollamada: !prevState.switchVideollamada,
-      }),
-      () => {
-        if (this.state.switchVideollamada) {
-          const uuid = createUUID();
-          const encriptada = CryptoJS.AES.encrypt(uuid, secretKey).toString();
-          this.setState({ idSala: encriptada });
-        }
-      }
-    );
+    onClaseGuardada();
   };
 
   return (
@@ -63,7 +70,7 @@ const FormClase = ({ toggleModal, onClaseAgregada, subject, user }) => {
           as={Input}
           control={control}
           name="nombre"
-          defaultValue=""
+          defaultValue={nombre || ''}
           rules={{
             required: { value: true, message: 'El nombre es requerido' },
           }}
@@ -81,7 +88,7 @@ const FormClase = ({ toggleModal, onClaseAgregada, subject, user }) => {
           as={Input}
           control={control}
           name="fecha"
-          defaultValue=""
+          defaultValue={fecha || ''}
           type="date"
           placeholder="DD/MM/AAAA"
           rules={{
@@ -99,7 +106,7 @@ const FormClase = ({ toggleModal, onClaseAgregada, subject, user }) => {
           as={Input}
           control={control}
           name="descripcion"
-          defaultValue=""
+          defaultValue={descripcion || ''}
           type="textarea"
           rules={{
             required: { value: true, message: 'La descripción es requerida' },
@@ -112,22 +119,22 @@ const FormClase = ({ toggleModal, onClaseAgregada, subject, user }) => {
         )}
       </FormGroup>
 
-      <FormGroup check>
-        <Label check>¿Esta clase tendrá videollamada?</Label>
+      <FormGroup className="form-check-switch">
+        <Label>¿Esta clase tendrá videollamada?</Label>
         <Switch
+          checked={switchVideollamada}
           id="Tooltip-Switch"
           className="custom-switch custom-switch-primary"
           onChange={(value) => {
-            console.log(value);
             setSwitchVideollamada(value);
           }}
           checkedChildren="Si"
           unCheckedChildren="No"
         />
       </FormGroup>
-      <ModalFooter>
+      <ModalFooter className="card-notas">
         <Button color="primary" type="submit">
-          Agregar
+          {idClase ? 'Editar' : 'Agregar'}
         </Button>
         <Button color="secondary" onClick={toggleModal}>
           Cancelar
