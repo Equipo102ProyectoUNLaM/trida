8b71868aa2d3e4399fdf76336af744a8a4a34966
Thomas Reynoso <thomas@mudafy.com>
2020-09-11 00:41:11 -0300

    limpieza v2

--- front/src/pages/app/clases-virtuales/clases/detalle-clase/modal-crear-preguntas.js
+++ front/src/pages/app/clases-virtuales/clases/detalle-clase/modal-crear-preguntas.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import { Separator } from 'components/common/CustomBootstrap';
-import { Row, Button, FormGroup, Label, ModalBody, NavLink } from 'reactstrap';
-import { Formik, Form, Field } from 'formik';
-import { editDocument } from 'helpers/Firebase-db';
-import { isEmpty } from 'helpers/Utils';
-import IntlMessages from 'helpers/IntlMessages';
+import { ModalBody } from 'reactstrap';
 import FormPreguntas from '../preguntas-clase/form-preguntas';
 
 class ModalCrearPreguntas extends React.Component {
@@ -12,25 +7,18 @@ class ModalCrearPreguntas extends React.Component {
     super(props);
 
     this.state = {
-      //isLoading: true,
       esPregunta: true,
-      nombre: '',
-      fecha_creacion: '',
-      fecha_publicacion: '',
-      fecha_finalizacion: '',
-      descripcion: '',
       ejercicios: [],
     };
   }
 
-  //ver si las funciones conviene declararlas acá
   render() {
     const {
       isLoading,
       idClase,
+      preguntas,
       toggleModalPreguntas,
       updatePreguntas,
-      preguntas,
     } = this.props;
 
     return isLoading ? (
@@ -40,7 +28,6 @@ class ModalCrearPreguntas extends React.Component {
     ) : (
       <>
         <FormPreguntas
-          evaluacion={this.state}
           preguntas={preguntas}
           idClase={idClase}
           toggleModalPreguntas={toggleModalPreguntas}
