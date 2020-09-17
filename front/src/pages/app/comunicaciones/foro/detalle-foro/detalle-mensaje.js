b82344b361fc7798fa72277d2f985402079afcf1
julietafoglia <julieta.foglia@valtech.com>
2020-09-17 12:37:41 -0300

    feature/DEV-228: secambia forma de traer mensajes

--- front/src/pages/app/comunicaciones/foro/detalle-foro/detalle-mensaje.js
+++ front/src/pages/app/comunicaciones/foro/detalle-foro/detalle-mensaje.js
@@ -6,26 +6,26 @@ const DetalleMensaje = ({ item, idUsuarioActual }) => {
     <Fragment>
       <Card
         className={`d-inline-block mb-3 float-${
-          item.creador !== idUsuarioActual ? 'left' : 'right'
+          item.data.creador !== idUsuarioActual ? 'left' : 'right'
         }`}
       >
         <div className="position-absolute  pt-1 pr-2 r-0">
           <span className="text-extra-small text-muted">
-            {item.fecha_creacion}
+            {item.data.fecha_creacion}
           </span>
         </div>
         <CardBody>
           <div className="d-flex flex-row pb-1">
             <img
-              alt={item.nombreCreador}
-              src={item.fotoCreador}
+              alt={item.data.nombreCreador}
+              src={item.data.fotoCreador}
               className="img-thumbnail border-0 rounded-circle mr-3 list-thumbnail align-self-center xsmall"
             />
             <div className=" d-flex flex-grow-1 min-width-zero">
               <div className="m-2 pl-0 align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero">
                 <div className="min-width-zero">
                   <p className="mb-0 truncate list-item-heading">
-                    {item.nombreCreador}
+                    {item.data.nombreCreador}
                   </p>
                 </div>
               </div>
@@ -33,7 +33,7 @@ const DetalleMensaje = ({ item, idUsuarioActual }) => {
           </div>
 
           <div className="chat-text-left">
-            <p className="mb-0 text-semi-muted">{item.contenido}</p>
+            <p className="mb-0 text-semi-muted">{item.data.contenido}</p>
           </div>
         </CardBody>
       </Card>
