86189fab2db868f7f5f613a4dd7836c42c32a59d
julietafoglia <julieta.foglia@valtech.com>
2020-09-29 20:29:46 -0300

    feature/DEV-270: se cambian estilos de modal de detalle mensaje

--- front/src/containers/pages/ModalConfirmacion.js
+++ front/src/containers/pages/ModalConfirmacion.js
@@ -21,29 +21,39 @@ export default class ModalConfirmacion extends Component {
       fecha,
       usuarios,
       esEnviado,
+      modalFooterClassname,
     } = this.props;
     return (
-      <Modal isOpen={isOpen} toggle={toggle}>
+      <Modal
+        isOpen={isOpen}
+        toggle={toggle}
+        className={modalFooterClassname ? 'modal-detalle-mensaje' : ''}
+      >
         <ModalHeader toggle={toggle}>
           <Row className="title">{titulo}</Row>
           {fecha && (
-            <Row className="text-muted small fecha-mensaje"> {fecha}</Row>
+            <Row className="text-muted small align-items-center">
+              <i className="iconsminds-endways mr-1" />
+              {fecha}
+            </Row>
           )}
         </ModalHeader>
         <ModalBody>
-          <Row className="break-word">{texto}</Row>
+          <Row className="break-word texto-mensaje">{texto}</Row>
           {usuarios && esEnviado && (
             <Row className="text-muted users-names">
+              <i className="iconsminds-mail-with-at-sign mr-1 icono-mensaje" />
               Destinatarios: {usuarios}{' '}
             </Row>
           )}
           {usuarios && !esEnviado && (
             <Row className="text-muted users-names">
+              <i className="iconsminds-mail-with-at-sign mr-1 icono-mensaje" />
               Enviado por: {usuarios}{' '}
             </Row>
           )}
         </ModalBody>
-        <ModalFooter>
+        <ModalFooter className={modalFooterClassname}>
           {buttonPrimary && (
             <Button color="primary" size="sm" onClick={onConfirm}>
               {buttonPrimary}
