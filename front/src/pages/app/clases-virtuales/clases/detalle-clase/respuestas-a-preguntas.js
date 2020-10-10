fa469dc72108c07428ac41b203f274105db6f01c
Thomas Reynoso <thomas@mudafy.com>
2020-10-10 19:17:29 -0300

    se guardan nombre de las opciones del choice

--- front/src/pages/app/clases-virtuales/clases/detalle-clase/respuestas-a-preguntas.js
+++ front/src/pages/app/clases-virtuales/clases/detalle-clase/respuestas-a-preguntas.js
@@ -40,27 +40,18 @@ const RespuestasAPreguntas = ({
   });
 
   const crearCantRespuestasPorPregunta = () => {
-    /*       let respuestas = []
-      let obj = [{ }];
-      let i = 0; //idx de cada pregunta
-      for (const pregunta of preguntasConRespuestasDeAlumnos) {
-        console.log("pregunta",  pregunta);
-        obj[i].consgina = pregunta.data.base.consigna;
-        for (const respuestas of pregunta.data.subcollections ) {
-          console.log("respuestas", respuestas);
-        }
-        
-      }
-
-      console.log("obj", obj);
-*/
     const resumen = [];
     if (preguntasConRespuestasDeAlumnos.length > 0) {
       preguntasConRespuestasDeAlumnos.forEach((preg) => {
         let resultado = [];
+        let opciones = [];
         for (let i = 0; i < preg.data.base.opciones.length; i++) {
+          opciones.push(preg.data.base.opciones[i].opcion);
           resultado.push(0);
         }
+        const rtasVerdaderas = preg.data.base.opciones.findIndex(
+          (opc) => opc.verdadera
+        );
         preg.data.subcollections.forEach((sub) => {
           sub.data.respuestas.forEach((res) => {
             resultado[res]++;
@@ -69,10 +60,12 @@ const RespuestasAPreguntas = ({
         resumen.push({
           id: preg.id,
           consigna: preg.data.base.consigna,
-          resultados: resultado,
+          resultado: resultado,
+          opciones: opciones,
+          respuestasVerdaderas: rtasVerdaderas,
         });
       });
-      console.log('resumen', resumen);
+      setRespuestasPorPregunta(resumen);
     }
   };
 
