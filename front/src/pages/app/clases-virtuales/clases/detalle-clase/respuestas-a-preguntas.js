c9b11ca64f8d2c4cf8544c8d368e3c193a5308ed
Thomas Reynoso <thomas@mudafy.com>
2020-10-11 16:20:47 -0300

    se marca cual es la opcion correcta y se dan estilos a las cards

--- front/src/pages/app/clases-virtuales/clases/detalle-clase/respuestas-a-preguntas.js
+++ front/src/pages/app/clases-virtuales/clases/detalle-clase/respuestas-a-preguntas.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Card, CardBody, CardTitle, Progress } from 'reactstrap';
+import { Card, CardBody, CardTitle, Progress, Badge } from 'reactstrap';
 
 import IntlMessages from 'helpers/IntlMessages';
 import {
@@ -9,31 +9,14 @@ import {
 } from 'helpers/Firebase-db';
 import { desencriptarPreguntasConRespuestasDeAlumnos } from 'handlers/DecryptionHandler';
 
-const RespuestasAPreguntas = ({ isLoading, idClase, cardClass = 'h-100' }) => {
+const RespuestasAPreguntas = ({ isLoading, idClase }) => {
   const [respuestasPorPregunta, setRespuestasPorPregunta] = useState([]);
   const [isLoadingLocal, setIsLoadingLocal] = useState(isLoading);
   const [
     preguntasConRespuestasDeAlumnos,
     setPreguntasConRespuestasDeAlumnos,
   ] = useState([]);
-  var preguntasConRespuestasDeAlumnosGlobal = [];
-  const data = [
-    {
-      title: 'Opción 1',
-      total: 18,
-      status: 12,
-    },
-    {
-      title: 'Opción 2',
-      total: 8,
-      status: 1,
-    },
-    {
-      title: 'Opción 3',
-      total: 6,
-      status: 2,
-    },
-  ];
+  var preguntasConRespuestasDeAlumnosGlobal = []; // ver de sacarla
 
   useEffect(() => {
     getPreguntasConRespuestasDeAlumnos();
@@ -68,16 +51,19 @@ const RespuestasAPreguntas = ({ isLoading, idClase, cardClass = 'h-100' }) => {
       preguntasConRespuestasDeAlumnosGlobal.forEach((preg) => {
         let resultado = [];
         let opciones = [];
+        let cantTotalRtas = 0;
         for (let i = 0; i < preg.data.base.opciones.length; i++) {
           opciones.push(preg.data.base.opciones[i].opcion);
           resultado.push(0);
         }
-        const rtasVerdaderas = preg.data.base.opciones.findIndex(
-          (opc) => opc.verdadera
-        );
+        const rtasVerdaderas = preg.data.base.opciones
+          .map((opc, idx) => (opc.verdadera ? idx : ''))
+          .filter(String);
+
         preg.data.subcollections.forEach((sub) => {
           sub.data.respuestas.forEach((res) => {
             resultado[res]++;
+            cantTotalRtas++;
           });
         });
         resumen.push({
@@ -86,6 +72,7 @@ const RespuestasAPreguntas = ({ isLoading, idClase, cardClass = 'h-100' }) => {
           resultado: resultado,
           opciones: opciones,
           respuestasVerdaderas: rtasVerdaderas,
+          cantTotalRtas: cantTotalRtas,
         });
       });
       console.log(resumen);
@@ -97,26 +84,37 @@ const RespuestasAPreguntas = ({ isLoading, idClase, cardClass = 'h-100' }) => {
   return isLoadingLocal ? (
     <div className="loading" />
   ) : (
-    <Card className={cardClass}>
-      <CardBody>
-        <CardTitle>
-          <IntlMessages id="clase.ver-respuestas" />
-        </CardTitle>
-        {data.map((s, index) => {
-          return (
-            <div key={index} className="mb-4">
-              <p className="mb-2">
-                {s.title}
-                <span className="float-right text-muted">
-                  {s.status}/{s.total}
-                </span>
-              </p>
-              <Progress value={(s.status / s.total) * 100} />
-            </div>
-          );
-        })}
-      </CardBody>
-    </Card>
+    <>
+      {respuestasPorPregunta.map((rta, idx) => {
+        return (
+          <Card key={idx} className="h-100 card-respuestas">
+            <CardBody>
+              <CardTitle>{rta.consigna}</CardTitle>
+              {rta.resultado.map((s, index) => {
+                return (
+                  <div key={index} className="mb-4">
+                    <div className="mb-2">
+                      <div className="prueba">
+                        {rta.opciones[index]}
+                        {rta.respuestasVerdaderas.includes(index) && (
+                          <Badge color="danger" pill>
+                            Correcta
+                          </Badge>
+                        )}
+                      </div>
+                      <span className="float-right text-muted">
+                        {s}/{rta.cantTotalRtas}
+                      </span>
+                    </div>
+                    <Progress value={(s / rta.cantTotalRtas) * 100} />
+                  </div>
+                );
+              })}
+            </CardBody>
+          </Card>
+        );
+      })}
+    </>
   );
 };
 
