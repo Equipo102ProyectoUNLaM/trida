722a266866def191d05956beb7c2af62b1118ef3
Lautaro <lperez@lagraninmobiliaria.com>
2020-10-21 00:43:16 -0300

    remove comments

--- front/src/pages/app/comunicaciones/foro/form-foro.js
+++ front/src/pages/app/comunicaciones/foro/form-foro.js
@@ -1,112 +1,13 @@
 import { editDocument, addDocument } from 'helpers/Firebase-db';
-import { capitalizeString } from 'helpers/Utils';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ModalFooter, Button, FormGroup, Label, Row } from 'reactstrap';
 import Select from 'react-select';
 import { Colxx } from 'components/common/CustomBootstrap';
 import IntlMessages from 'helpers/IntlMessages';
-import ROLES from 'constants/roles';
 import { Formik, Form, Field } from 'formik';
 import { mensajesSchema } from './validations';
 import { createUserList } from 'helpers/Firebase-user';
-
-// const FormForo = ({
-//   toggleModal,
-//   onForoGuardado,
-//   subject,
-//   user,
-//   idForo,
-//   nombre,
-//   descripcion,
-//   mensajes,
-// }) => {
-//   const { handleSubmit, errors, control } = useForm();
-
-//   const onSubmit = async (values) => {
-//     const { nombre, descripcion } = values;
-
-//     const obj = {
-//       nombre: capitalizeString(nombre),
-//       descripcion,
-//       idMateria: subject.id,
-//     };
-
-//     if (idForo) {
-//       await editDocument(
-//         'foros',
-//         idForo,
-//         obj,
-//         'Tema editado',
-//         'Tema editado',
-//         'Error al guardar el tema'
-//       );
-//     } else {
-//       await addDocument(
-//         'foros',
-//         obj,
-//         user,
-//         'Tema agregado',
-//         'Tema agregado exitosamente',
-//         'Error al agregar el tema'
-//       );
-//     }
-
-//     onForoGuardado();
-//   };
-
-//   return (
-//     <form
-//       className="av-tooltip tooltip-label-right"
-//       onSubmit={handleSubmit(onSubmit)}
-//     >
-//       <FormGroup className="mb-3 error-l-150">
-//         <Label>Nombre del tema</Label>
-//         <Controller
-//           as={Input}
-//           control={control}
-//           name="nombre"
-//           defaultValue={nombre || ''}
-//           rules={{
-//             required: { value: true, message: 'El nombre es requerido' },
-//           }}
-//         />
-//         {errors.nombre && (
-//           <div className="invalid-feedback d-block">
-//             {errors.nombre.message}
-//           </div>
-//         )}
-//       </FormGroup>
-
-//       <FormGroup className="mb-3 error-l-150">
-//         <Label>Descripción</Label>
-//         <Controller
-//           as={Input}
-//           control={control}
-//           name="descripcion"
-//           defaultValue={descripcion || ''}
-//           type="textarea"
-//           rules={{
-//             required: { value: true, message: 'La descripción es requerida' },
-//           }}
-//         />
-//         {errors.descripcion && (
-//           <div className="invalid-feedback d-block">
-//             {errors.descripcion.message}
-//           </div>
-//         )}
-//       </FormGroup>
-//       <ModalFooter className="card-notas">
-//         <Button color="primary" type="submit">
-//           {idForo ? 'Editar' : 'Agregar'}
-//         </Button>
-//         <Button color="secondary" onClick={toggleModal}>
-//           Cancelar
-//         </Button>
-//       </ModalFooter>
-//     </form>
-//   );
-// };
 
 class FormForo extends Component {
   constructor(props) {
