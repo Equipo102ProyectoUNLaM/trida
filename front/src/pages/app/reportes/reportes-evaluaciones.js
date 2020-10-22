05c8c9145d1d1afcf47ff12ac3bf612090756927
Lautaro <lperez@lagraninmobiliaria.com>
2020-10-22 10:55:46 -0300

    remove unuseful imports

--- front/src/pages/app/reportes/reportes-evaluaciones.js
+++ front/src/pages/app/reportes/reportes-evaluaciones.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from 'react';
 import { Row } from 'reactstrap';
-import IntlMessages from 'helpers/IntlMessages';
 import { Colxx, Separator } from 'components/common/CustomBootstrap';
 import Breadcrumb from 'containers/navegacion/Breadcrumb';
 
@@ -10,16 +9,15 @@ export default class ReportesEvaluaciones extends Component {
       <Fragment>
         <Row>
           <Colxx xxs="12">
-            <Breadcrumb heading="menu.blank-page" match={this.props.match} />
+            <Breadcrumb
+              heading="menu.my-reportes-evaluaciones"
+              match={this.props.match}
+            />
             <Separator className="mb-5" />
           </Colxx>
         </Row>
         <Row>
-          <Colxx xxs="12" className="mb-4">
-            <p>
-              <IntlMessages id="menu.my-reportes-evaluaciones" />
-            </p>
-          </Colxx>
+          <Colxx xxs="12" className="mb-4"></Colxx>
         </Row>
       </Fragment>
     );
--- front/src/pages/app/reportes/reportes-generales.js
+++ front/src/pages/app/reportes/reportes-generales.js
@@ -1,6 +1,5 @@
 import React, { Component, Fragment } from 'react';
 import { Row } from 'reactstrap';
-import IntlMessages from 'helpers/IntlMessages';
 import { Colxx, Separator } from 'components/common/CustomBootstrap';
 import Breadcrumb from 'containers/navegacion/Breadcrumb';
 
@@ -10,16 +9,15 @@ export default class ReportesGenerales extends Component {
       <Fragment>
         <Row>
           <Colxx xxs="12">
-            <Breadcrumb heading="menu.blank-page" match={this.props.match} />
+            <Breadcrumb
+              heading="menu.my-reportes-generales"
+              match={this.props.match}
+            />
             <Separator className="mb-5" />
           </Colxx>
         </Row>
         <Row>
-          <Colxx xxs="12" className="mb-4">
-            <p>
-              <IntlMessages id="menu.my-reportes-generales" />
-            </p>
-          </Colxx>
+          <Colxx xxs="12" className="mb-4"></Colxx>
         </Row>
       </Fragment>
     );
