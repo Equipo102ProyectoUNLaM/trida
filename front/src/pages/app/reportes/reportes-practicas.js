8f7be250610dc63f278da6a47321e0b69c1f4f77
julietafoglia <julieta.foglia@valtech.com>
2020-10-23 16:14:31 -0300

    feature/DEV-325: se agrega reporte de practicas

--- front/src/pages/app/reportes/reportes-practicas.js
+++ front/src/pages/app/reportes/reportes-practicas.js
@@ -1,12 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
-import { Row, Col, Container } from 'reactstrap';
+import { Row } from 'reactstrap';
 import { groupBy } from 'lodash';
 import { Colxx, Separator } from 'components/common/CustomBootstrap';
 import Breadcrumb from 'containers/navegacion/Breadcrumb';
 import { getCollection, getDocument } from 'helpers/Firebase-db';
+import { isEmpty } from 'helpers/Utils';
 
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -14,6 +14,11 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
 class ReportesPracticas extends Component {
   constructor(props) {
@@ -21,12 +26,25 @@ class ReportesPracticas extends Component {
 
     this.state = {
       dataPracticas: [],
+      open: [],
     };
   }
 
   componentDidMount() {
     this.getCorreccionesDePracticas();
   }
+
+  toggleOpen = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  toggleAccordion = (tab) => {
+    const prevState = this.state.open;
+    const state = prevState.map((x, index) => (tab === index ? !x : false));
+    this.setState({
+      open: state,
+    });
+  };
 
   getNombreUsuario = async (id) => {
     const { data } = await getDocument(`usuarios/${id}`);
@@ -58,20 +76,25 @@ class ReportesPracticas extends Component {
         nombreUsuario: await this.getNombreUsuario(elem.data.idUsuario),
         estado: elem.data.estadoCorreccion,
         nota: elem.data.notaCorreccion,
+        fecha: elem.data.fecha_creacion,
       };
     });
 
     let dataPracticas = await Promise.all(dataPracticasPromise);
-    /* dataPracticas = groupBy(dataPracticas, 'nombreUsuario');
+    let openData = [];
+    dataPracticas = groupBy(dataPracticas, 'nombreUsuario');
     dataPracticas = Object.entries(dataPracticas).map((elem) => {
+      openData.push(false);
       return { id: elem[0], data: elem[1] };
-    }); */
-    this.setState({ dataPracticas });
+    });
+    this.setState({ dataPracticas, open: openData });
   };
 
   render() {
-    const { dataPracticas } = this.state;
-    return (
+    const { dataPracticas, open } = this.state;
+    return isEmpty(dataPracticas) ? (
+      <div className="loading" />
+    ) : (
       <Fragment>
         <Row>
           <Colxx xxs="12">
@@ -79,58 +102,71 @@ class ReportesPracticas extends Component {
             <Separator className="mb-5" />
           </Colxx>
         </Row>
-        {/*  <Row>
-          <Row className="display-row" xs="1" sm="2" md="4">
-            <Col>Alumno</Col>
-            <Col>Práctica</Col>
-            <Col>Estado</Col>
-            <Col>Nota</Col>
-          </Row>
-          {dataPracticas.map((elem) => {
-            return (
-              <Container key={elem.id}>
-                <Row className="display-row">
-                  <Col>{elem.id}</Col>
-                  {elem.data.map((data) => {
-                    return (
-                      <Row className="display-row" key={data.id}>
-                        <Col>{data.nombrePractica}</Col>
-                        <Col>{data.estado}</Col>
-                        <Col>{data.nota}</Col>
-                      </Row>
-                    );
-                  })}
-                </Row>
-              </Container>
-            );
-          })}
-        </Row> */}
-        <Separator />
         <TableContainer component={Paper}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Alumno</TableCell>
-                <TableCell>Práctica</TableCell>
-                <TableCell>Estado</TableCell>
-                <TableCell>Nota</TableCell>
+          {dataPracticas.map((row, index) => (
+            <Fragment key={index}>
+              <TableRow className="mb-2">
+                <TableCell className="button-toggle">
+                  <IconButton
+                    aria-label="expand row"
+                    size="small"
+                    onClick={() => this.toggleAccordion(index)}
+                  >
+                    {open ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}
+                  </IconButton>
+                </TableCell>
+                <TableCell className="width-100 font-weight-bold">
+                  {row.id}
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {dataPracticas.map((row) => {
-                return (
-                  <TableRow key={row.id}>
-                    <TableCell component="th" scope="row">
-                      {row.nombreUsuario}
-                    </TableCell>
-                    <TableCell>{row.nombrePractica}</TableCell>
-                    <TableCell>{row.estado}</TableCell>
-                    <TableCell>{row.nota}</TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
+              <TableRow>
+                <TableCell
+                  style={{ paddingBottom: 0, paddingTop: 0 }}
+                  colSpan={5}
+                  className="padding-left-inner"
+                >
+                  <Collapse in={open[index]} timeout="auto">
+                    <Box margin={1}>
+                      <Table
+                        className="data-entregas"
+                        size="small"
+                        aria-label="entregas"
+                      >
+                        <TableHead>
+                          <TableRow>
+                            <TableCell className="font-weight-bold">
+                              Práctica
+                            </TableCell>
+                            <TableCell className="font-weight-bold">
+                              Fecha de Entrega
+                            </TableCell>
+                            <TableCell className="font-weight-bold">
+                              Estado
+                            </TableCell>
+                            <TableCell className="font-weight-bold">
+                              Nota
+                            </TableCell>
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {row.data.map((historyRow) => (
+                            <TableRow key={historyRow.id}>
+                              <TableCell component="th" scope="row">
+                                {historyRow.nombrePractica}
+                              </TableCell>
+                              <TableCell>{historyRow.fecha}</TableCell>
+                              <TableCell>{historyRow.estado}</TableCell>
+                              <TableCell>{historyRow.nota}</TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </Box>
+                  </Collapse>
+                </TableCell>
+              </TableRow>
+            </Fragment>
+          ))}
         </TableContainer>
       </Fragment>
     );
