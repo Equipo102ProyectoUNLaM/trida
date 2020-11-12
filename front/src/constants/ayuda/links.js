aeeb670fe84572cc2370b1b70b3849b019561f45
julietafoglia <julieta.foglia@valtech.com>
2020-11-12 14:54:12 -0300

    videos clase alumno

--- front/src/constants/ayuda/links.js
+++ front/src/constants/ayuda/links.js
@@ -10,9 +10,10 @@ const LINKS = {
         'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FabmClase.mp4?alt=media&token=52e87528-f412-42c0-84b5-e7579ad6a2df',
       CLASE_VIRTUAL:
         'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FclaseVirtual.mp4?alt=media&token=42101383-67bc-4d6a-83be-90fe7a7ed91a',
-      PREGUNTAS_CLASE: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+      PREGUNTAS_CLASE:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FpreguntasClaseDocente.mp4?alt=media&token=5cbe2ebd-de79-46a1-91fb-b65afdabbc82',
       PREGUNTAS_ALUMNO:
-        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FpreguntaAlumno.mp4?alt=media&token=bfb8b30a-a248-4218-9b70-fdd428f4b953',
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FpreguntaAlumno.mp4?alt=media&token=292e7e38-482c-473c-955b-6b7c364a9766',
       CONTENIDOS:
         'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2Fcontenidos.mp4?alt=media&token=e551493b-8b54-4e7e-a65e-ada0eb1a5fbd',
       ASISTENCIA:
@@ -58,10 +59,14 @@ const LINKS = {
       NOTIFICACIONES: 'https://www.youtube.com/embed/E07s5ZYygMg',
     },
     CLASES: {
-      CLASE_VIRTUAL: 'https://www.youtube.com/embed/_tG70FWd1Ds',
-      PREGUNTAS_CLASE: 'https://www.youtube.com/embed/_tG70FWd1Ds',
-      PREGUNTAS_ALUMNO: 'https://www.youtube.com/embed/_tG70FWd1Ds',
-      CONTENIDOS: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+      CLASE_VIRTUAL:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FclaseAlumno.mp4?alt=media&token=bd013aed-e1f5-4897-9558-88a6a1f54054',
+      PREGUNTAS_CLASE:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FpreguntasClaseAlumno.mp4?alt=media&token=c23dd720-9984-41c0-8ce8-af3a277cf6de',
+      PREGUNTAS_ALUMNO:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FpreguntaAnonimaAlumno.mp4?alt=media&token=34a41527-6b78-4bf0-add6-aa8b5e8c6aa1',
+      CONTENIDOS:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FalumnoContenidos.mp4?alt=media&token=0489c285-6ed9-4838-9e16-28f78c6949d4',
     },
     EVALUACIONES: {
       REALIZAR: 'https://www.youtube.com/embed/_tG70FWd1Ds',
