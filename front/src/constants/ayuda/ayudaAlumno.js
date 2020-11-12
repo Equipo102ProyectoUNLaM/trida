05284b0d978e9155bafb550ac120a8b33366a3d0
julietafoglia <julieta.foglia@valtech.com>
2020-11-12 13:48:22 -0300

    Se agregan constantes de links de videos

--- front/src/constants/ayuda/ayudaAlumno.js
+++ front/src/constants/ayuda/ayudaAlumno.js
@@ -1,60 +1,72 @@
-const ayudaDocente = [
+import LINKS from './links';
+
+const ayudaAlumno = [
   {
     question: 'General',
     answer: `<strong>Menú</strong><br/>
-          En el menú lateral encontrarás cada uno de los módulos con los que cuenta la aplicación. Para desplegarlo, clickeá el ícono que se encuentra a la izquierda del nombre de tu institución.<br/><br/>
-          <strong>Notificaciones</strong><br/>
-          En la barra superior podrás ver notificaciones cada vez que recibas un mensaje directo, una práctica, una evaluación o una corrección. Para verlas, clickeá el ícono de la campana.<br/><br/>
-      `,
+    En el menú lateral encontrarás cada uno de los módulos con los que cuenta la aplicación. Para desplegarlo, clickeá el ícono que se encuentra a la izquierda del nombre de tu institución.<br/><br/>
+    <iframe width="560" height="315" src=${LINKS.ALUMNO.GENERAL.MENU} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+   
+    <strong>Barra Superior</strong><br/>
+    En la barra superior podrás ver distintas opciones para personalizar la aplicación y tu cuenta.<br/><br/>
+    <iframe width="560" height="315" src=${LINKS.ALUMNO.GENERAL.TOP_NAV} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+
+    <strong>Notificaciones</strong><br/>
+    En la barra superior podrás ver notificaciones cada vez que recibas un mensaje directo o un archivo para corregir. Para verlas, clickeá el ícono de la campana.<br/><br/>
+    <iframe width="560" height="315" src=${LINKS.ALUMNO.GENERAL.NOTIFICACIONES} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+`,
   },
   {
     question: 'Clases Virtuales',
     answer: `En el módulo de clases virtuales podrás ingresar a las clases cargadas por tu docente. Podrás realizar consultas anónimas y escritas durante la clase. Además, podrás ver contenidos asociados a la clase.<br/><br/>
         <strong>Clase Virtual</strong><br/><br/>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/xWkKAbENwAs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+        <iframe width="560" height="315" src=${LINKS.ALUMNO.CLASES.CLASE_VIRTUAL} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
         <strong>Preguntas de la Clase</strong><br/><br/>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/8j1xiiAZhIQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+        <iframe width="560" height="315" src=${LINKS.ALUMNO.CLASES.PREGUNTAS_CLASE} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
         <strong>Preguntas Anónimas</strong><br/><br/>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/8j1xiiAZhIQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+        <iframe width="560" height="315" src=${LINKS.ALUMNO.CLASES.PREGUNTAS_ALUMNO} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
         <strong>Contenidos de Clase</strong><br/><br/>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/rzgw0XldTC0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+        <iframe width="560" height="315" src=${LINKS.ALUMNO.CLASES.CONTENIDOS} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
         `,
   },
   {
     question: 'Evaluaciones',
     answer: `En el módulo de evaluaciones podrás realizar las evaluaciones cargadas por tu docente. Prestá atención a las características de la evaluación cuando clickees 'Realizar', ya que tu docente puede haber seleccionado que no puedas navegar fuera de la evaluación o no puedas tomar capturas de pantalla.<br/><br/>
                   <strong>Realizar Evaluación</strong><br/><br/>
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/E07s5ZYygMg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+                  <iframe width="560" height="315" src=${LINKS.ALUMNO.EVALUACIONES.REALIZAR} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
            `,
   },
   {
     question: 'Prácticas',
-    answer: `En este módulo podrás crear y editar prácticas. Serán visibles por tus alumnos en la fecha que elijas como lanzamiento.<br/><br/>
-        <strong>Crear y Editar Prácticas</strong><br/><br/>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/XseIJg8Vyj0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+    answer: `En este módulo podrás subir las prácticas cargadas por tu docente.<br/><br/>
+        <strong>Subir Prácticas</strong><br/><br/>
+        <iframe width="560" height="315" src=${LINKS.ALUMNO.PRACTICAS.SUBIR} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
       `,
   },
   {
     question: 'Correcciones',
     answer: `En el módulo de correcciones podrás ver las correcciones realizadas por tu docente a las prácticas y evaluaciones que le envíes.<br/><br/>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/GkkPazmc-uk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+        <iframe width="560" height="315" src=${LINKS.ALUMNO.CORRECCIONES.VER} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
       `,
   },
   {
     question: 'Contenidos',
     answer: `En el módulo de Contenidos podrás ver y descargar contenidos cargados por tu docente.<br /><br/>
-                  <iframe width="560" height="315" src="https://www.youtube.com/embed/YSoT3T58QFY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+                  <iframe width="560" height="315" src=${LINKS.ALUMNO.CONTENIDOS.VER} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
     `,
   },
   {
     question: 'Comunicaciones',
     answer: `En el módulo de Comunicaciones podrás interactuar con tus compañeros y tu docente por distintos canales.<br /><br/>
         <strong>Foro</strong><br/><br/>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/BNjkUgeWwPQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+        <iframe width="560" height="315" src=${LINKS.ALUMNO.COMUNICACIONES.FORO} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
         <strong>Mensajería</strong><br/><br/>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/LoHHCJMdMuQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+        <iframe width="560" height="315" src=${LINKS.ALUMNO.COMUNICACIONES.MENSAJERIA} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+        <strong>Comunicaciones Formales</strong><br/><br/>
+        <iframe width="560" height="315" src=${LINKS.ALUMNO.COMUNICACIONES.FORMALES} frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br/><br/>
+       
           `,
   },
 ];
 
-export default ayudaDocente;
+export default ayudaAlumno;
---
+++ front/src/constants/ayuda/links.js
@@ -0,0 +1,86 @@
+const LINKS = {
+  DIR_DOC: {
+    GENERAL: {
+      MENU: 'https://www.youtube.com/embed/E07s5ZYygMg',
+      TOP_NAV: 'https://www.youtube.com/embed/E07s5ZYygMg',
+      NOTIFICACIONES: 'https://www.youtube.com/embed/E07s5ZYygMg',
+    },
+    CLASES: {
+      ABM:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FabmClase.mp4?alt=media&token=52e87528-f412-42c0-84b5-e7579ad6a2df',
+      CLASE_VIRTUAL:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FclaseVirtual.mp4?alt=media&token=42101383-67bc-4d6a-83be-90fe7a7ed91a',
+      PREGUNTAS_CLASE: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+      PREGUNTAS_ALUMNO:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2FpreguntaAlumno.mp4?alt=media&token=bfb8b30a-a248-4218-9b70-fdd428f4b953',
+      CONTENIDOS:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2Fcontenidos.mp4?alt=media&token=e551493b-8b54-4e7e-a65e-ada0eb1a5fbd',
+      ASISTENCIA:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Fclases%2Fasistencia.mp4?alt=media&token=dd3ab7fd-f298-4104-bc01-6d67b7af3d34',
+    },
+    EVALUACIONES: {
+      ABM: 'https://www.youtube.com/embed/E07s5ZYygMg',
+      IMPORTAR_EXPORTAR: 'https://www.youtube.com/embed/0LVdYyPH6ag',
+    },
+    PRACTICAS: {
+      ABM: 'https://www.youtube.com/embed/XseIJg8Vyj0',
+    },
+    CORRECCIONES: {
+      REALIZAR_CORRECCION: 'https://www.youtube.com/embed/GkkPazmc-uk',
+    },
+    CONTENIDOS: {
+      AGREGAR: 'https://www.youtube.com/embed/YSoT3T58QFY',
+      AUDIO: 'https://www.youtube.com/embed/R-jsYSvYH4o',
+      VIDEO: 'https://www.youtube.com/embed/O4KWxa65Gsk',
+    },
+    COMUNICACIONES: {
+      FORO: 'https://www.youtube.com/embed/BNjkUgeWwPQ',
+      MENSAJERIA: 'https://www.youtube.com/embed/LoHHCJMdMuQ',
+      FORMALES: 'https://www.youtube.com/embed/LoHHCJMdMuQ',
+    },
+    ADMINISTRACION: {
+      USUARIOS: 'https://www.youtube.com/embed/LoHHCJMdMuQ',
+      ENVIAR_INVITACION: 'https://www.youtube.com/embed/LoHHCJMdMuQ',
+      AGREGAR_CURSO: 'https://www.youtube.com/embed/LoHHCJMdMuQ',
+      ASIGNAR_CURSO: 'https://www.youtube.com/embed/LoHHCJMdMuQ',
+    },
+    REPORTES: {
+      TODOS:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Freportes%2FtodosLosReportes.mp4?alt=media&token=1ba2d7eb-7491-42e6-9004-43cd32351d2d',
+      MI_PLANILLA:
+        'https://firebasestorage.googleapis.com/v0/b/trida-7f28f.appspot.com/o/seccionAyuda%2Freportes%2FmiPlanilla.mp4?alt=media&token=0ed02990-2dd4-4fc0-b768-6cf1cde9019a',
+    },
+  },
+  ALUMNO: {
+    GENERAL: {
+      MENU: 'https://www.youtube.com/embed/E07s5ZYygMg',
+      TOP_NAV: 'https://www.youtube.com/embed/E07s5ZYygMg',
+      NOTIFICACIONES: 'https://www.youtube.com/embed/E07s5ZYygMg',
+    },
+    CLASES: {
+      CLASE_VIRTUAL: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+      PREGUNTAS_CLASE: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+      PREGUNTAS_ALUMNO: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+      CONTENIDOS: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+    },
+    EVALUACIONES: {
+      REALIZAR: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+    },
+    PRACTICAS: {
+      SUBIR: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+    },
+    CORRECCIONES: {
+      VER: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+    },
+    CONTENIDOS: {
+      VER: 'https://www.youtube.com/embed/_tG70FWd1Ds',
+    },
+    COMUNICACIONES: {
+      FORO: 'https://www.youtube.com/embed/BNjkUgeWwPQ',
+      MENSAJERIA: 'https://www.youtube.com/embed/LoHHCJMdMuQ',
+      FORMALES: 'https://www.youtube.com/embed/LoHHCJMdMuQ',
+    },
+  },
+};
+
+export default LINKS;
