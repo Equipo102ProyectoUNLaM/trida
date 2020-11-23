5e9ae8e0fb62935f5b1ab2de3f20b79a012c27cd
julietafoglia <julieta.foglia@valtech.com>
2020-11-23 11:49:38 -0300

    Se corrige creacion de inst

--- front/src/pages/course-selection/creacion/crear-institucion.js
+++ front/src/pages/course-selection/creacion/crear-institucion.js
@@ -4,6 +4,7 @@ import { Colxx } from 'components/common/CustomBootstrap';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logoutUser } from 'redux/actions';
+import ROLES from 'constants/roles';
 
 class CrearInstitution extends Component {
   constructor(props) {
@@ -23,21 +24,31 @@ class CrearInstitution extends Component {
     return (
       <Colxx xxs="12" className="mb-4 course-col-container">
         <Colxx>
-          <h3 className="text-center">
-            El usuario con el que ingresaste no posee instituciones asociadas.{' '}
-            <br /> Podés crear tu institución o cerrar sesión para ingresar con
-            otro usuario.
-          </h3>
+          {this.props.rol !== ROLES.Alumno && (
+            <h3 className="text-center">
+              El usuario con el que ingresaste no posee instituciones asociadas.{' '}
+              <br /> Podés crear tu institución o cerrar sesión para ingresar
+              con otro usuario.
+            </h3>
+          )}
+          {this.props.rol === ROLES.Alumno && (
+            <h3 className="text-center">
+              El usuario con el que ingresaste no posee instituciones asociadas.{' '}
+              <br /> Debés esperar que te asignen una institución para ingresar.
+            </h3>
+          )}
           <Row className="button-group-centered">
-            <Button
-              color="primary"
-              onClick={() =>
-                this.props.history.push('/seleccion-curso/crear-institucion')
-              }
-              className="button"
-            >
-              Crear Institución
-            </Button>
+            {this.props.rol !== ROLES.Alumno && (
+              <Button
+                color="primary"
+                onClick={() =>
+                  this.props.history.push('/seleccion-curso/crear-institucion')
+                }
+                className="button"
+              >
+                Crear Institución
+              </Button>
+            )}
             <Button
               color="primary"
               onClick={() => this.handleLogout()}
@@ -52,8 +63,16 @@ class CrearInstitution extends Component {
   }
 }
 
+const mapStateToProps = ({ authUser }) => {
+  const { userData } = authUser;
+  const { rol } = userData;
+  return {
+    rol,
+  };
+};
+
 export default withRouter(
-  connect(null, {
+  connect(mapStateToProps, {
     logoutUser,
   })(CrearInstitution)
 );
